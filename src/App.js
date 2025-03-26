ea2905ceffe9e04ddc645eaa1ddf37908589cb51
ahm10050 <ahmad.al.asadi@fokus.fraunhofer.de>
2025-03-26 11:19:40 +0100

    make header responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Header from './components/Header.js'
 import Main from './components/Main';
 import Footer from './components/Footer';
-import "bootstrap/dist/css/bootstrap.min.css";
 import './styles/App.scss'
 // import React from 'react'
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,32 @@
 import logo from '../assets/Logo.svg';
-import NavLink from 'react'
+import NavLink from 'react-router-dom'
 
 function Header() {
 
   const navItems = ['home', 'about', 'menu', 'reservations', 'order online', 'login']
 
   return (
-    <header className="d-flex w-100 justify-content-around">
-      <img src={logo} alt="logo" />
-      <nav className='navbar'>
-        <ul className="navbar-nav d-flex flex-row gap-4 justify-content-between">
-          {navItems.map((e) => (
-            <li className='nav-item'>
-                {/* switch to NavLinks */}
-              <a href="" className='nav-link'>{e.toUpperCase()}</a>
-            </li>
-          ))}
-        </ul>
+    <header className="d-flex justify-content-around">
+      <nav className="navbar w-100 navbar-expand-md navbar-light bg-light">
+        <div className="container-fluid">
+          <img src={logo} alt="logo" />
+          {/* <a className="navbar-brand" href="#">Navbar</a> */}
+          <div>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {navItems.map((e, index) => (
+                    <li className='nav-item' key={index}>
+                        {/* switch to NavLinks */}
+                      <a href="" className='nav-link'>{e.toUpperCase()}</a>
+                    </li>
+                  ))}
+              </ul>
+            </div>
+          </div>
+        </div>
       </nav>
     </header>
   );
