d5afda2bc902e23bd3c14a5a17e3a4ce9e84d048
ahm10050 <ahmad.al.asadi@fokus.fraunhofer.de>
2025-04-09 20:43:13 +0200

    API logic initial

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import Main from './components/Main';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout.js';
-import './styles/App.scss'
-// import React from 'react'
+import Main from './pages/Main.js';
+import BookingView from './pages/BookingView.js';
+import { useEffect, useState } from 'react';
+import './styles/App.scss';
 
 function App() {
+  const [availableTimes, setAvailableTimes] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  useEffect(() => {
+    const fetchTimes = async () => {
+      try {
+        // const times = await fetchAPI(selectedDate);
+        // console.log("TIMES:", times)
+        // setAvailableTimes(times);
+      } catch (error) {
+        console.error("Error fetching available times:", error);
+        setAvailableTimes([]); // fallback in case of error
+      }
+    };
+
+    fetchTimes();
+  }, [selectedDate]);
+
   return (
-    <>
+    <Router>
       <Layout>
-        <Main/>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route
+            path="/booking"
+            element={
+              <BookingView
+                availableTimes={availableTimes}
+                setSelectedDate={setSelectedDate}
+              />
+            }
+          />
+        </Routes>
       </Layout>
-    </>
+    </Router>
   );
 }
 
